4f0ae9a69b42319bbec8bdd99fd5da1aca6479d1
Garrett <cyph17@gmail.com>
2019-07-15 15:29:50 -0700

    All tests pass

---
+++ server/datasources.local.js
@@ -0,0 +1,28 @@
+module.exports = {
+  'db': {
+    'name': 'db',
+    'connector': 'memory',
+  },
+  'MongoDB': {
+    'url': 'mongodb://localhost:27017',
+    'name': 'MongoDB',
+    'connector': 'mongodb',
+  },
+};
+// {
+//     "db": {
+//       "name": "db",
+//       "connector": "memory"
+//     },
+//     "MongoDB": {
+//       "host": "",
+//       "port": 0,
+//       "url": "",
+//       "database": "",
+//       "password": "",
+//       "name": "MongoDB",
+//       "user": "",
+//       "useNewUrlParser": true,
+//       "connector": "mongodb"
+//     }
+//   }
